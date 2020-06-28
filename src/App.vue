f1b7d7127a2a4b15a8c1219b2ae4cdd3e927e973
zwind <jody1225@qq.com>
2020-06-28 09:18:28 +0800

    buy goods

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+<div>
+  <h1>hello {{title}}!</h1>
+  <ul>
+    <li v-for="(good,index) in goods">
+      {{index}}:
+      {{good.text}}
+
+      |
+
+      {{good.price}}
+      <button @click="addCart(index)">添加购物车</button>
+    </li>
+  </ul>
+  <table border="1">
+    <tr>
+    <td>商品</td>
+    <td>单价</td>
+    <td>价格</td>
+    <td>数量</td>
+    <td>操作</td>
+    </tr> 
+    <tr>
+      <tr v-for="c in cart">
+        <td>{{c.text}}</td>
+        <td>{{c.price}}</td>
+        <td>{{c.price * c.count}}</td>
+        <td>{{c.count}}</td>
+    </tr>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      title:'vue test',
+      goods:[
+        {text:"苹果",price:80},
+        {text:"香蕉",price:100},
+        {text:"西瓜",price:60},
+        {text:"芒果",price:120},
+      ],
+      cart:[]
+    }
+  },
+    methods:{
+    addCart(i){
+      console.log( '添加购物车' )
+      let item = this.goods[i]
+      let good = this.cart.find(v=>v.text==item.text)
+      if (good){
+        good.count  +=  1
+      }else{
+        this.cart.push({...item, count:1})
+      }
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  color: blue;
+}
+</style>
